3e608497df9f4143ccf8c0758e13f720e2d891d2
liinahamari <l1maginaire00@gmail.com>
2023-05-27 09:54:02 +0300

    Code cleanup at PopupImage.kt

--- app/src/main/java/dev/liinahamari/highlights/PopupImage.kt
+++ app/src/main/java/dev/liinahamari/highlights/PopupImage.kt
@@ -1,9 +1,10 @@
 package dev.liinahamari.highlights
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.content.Context.WINDOW_SERVICE
 import android.graphics.Color
-import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -15,37 +16,36 @@ import android.widget.PopupWindow
 import androidx.appcompat.widget.AppCompatImageView
 import com.bumptech.glide.Glide
 
-class ImagePopup : AppCompatImageView {
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-
-    fun show(imageUrl: String?, view: View) {
+class ImagePopup constructor(context: Context) : AppCompatImageView(context) {
+    @SuppressLint("InflateParams") fun show(imageUrl: String?, viewToAttachTo: View) {
         val layout =
-            (view.context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.popup, null)
-                .apply { setBackgroundColor(Color.BLACK) }
+            (viewToAttachTo.context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+                R.layout.popup,
+                null
+            ).apply { setBackgroundColor(Color.BLACK) }
         val imageView = layout.findViewById<View>(R.id.imageView) as ImageView
-        Glide.with(view.context).load(imageUrl).into(imageView)
+
+        Glide.with(viewToAttachTo.context).load(imageUrl).into(imageView)
+
         PopupWindow(layout, MATCH_PARENT, MATCH_PARENT, true).apply {
-            showAtLocation(view, Gravity.CENTER, 0, 0)
+            showAtLocation(viewToAttachTo, Gravity.CENTER, 0, 0)
             layout.findViewById<ImageView>(R.id.closeBtn).setOnClickListener { dismiss() }
-            dimBehind(this)
+            dimBehind()
             imageView.setOnClickListener { dismiss() }
         }
     }
 
-    companion object {
-        fun dimBehind(popupWindow: PopupWindow) {
-            val container: View = if (popupWindow.background == null) {
-                popupWindow.contentView.parent as View
-            } else {
-                popupWindow.contentView.parent.parent as View
-            }
-            with(popupWindow.contentView.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager) {
-                updateViewLayout(container, (container.layoutParams as LayoutParams).apply {
-                    flags = LayoutParams.FLAG_DIM_BEHIND
-                    dimAmount = 0.3f
-                })
-            }
+    private fun PopupWindow.dimBehind() {
+        val container: View = if (background == null) {
+            contentView.parent as View
+        } else {
+            contentView.parent.parent as View
+        }
+        with(contentView.context.getSystemService(WINDOW_SERVICE) as WindowManager) {
+            updateViewLayout(container, (container.layoutParams as LayoutParams).apply {
+                flags = LayoutParams.FLAG_DIM_BEHIND
+                dimAmount = 0.3f
+            })
         }
     }
 }
