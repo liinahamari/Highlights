a2326a8403b4f732f6b08375529716cb5e51a588
liinahamari <l1maginaire00@gmail.com>
2023-06-05 09:04:38 +0300

    Introduce ANR Watchdog

--- app/src/main/java/dev/liinahamari/highlights/App.kt
+++ app/src/main/java/dev/liinahamari/highlights/App.kt
@@ -1,6 +1,7 @@
 package dev.liinahamari.highlights
 
 import android.app.Application
+import com.github.anrwatchdog.ANRWatchDog
 import dev.liinahamari.highlights.di.components.DaggerMainComponent
 import dev.liinahamari.highlights.di.components.MainComponent
 
@@ -9,5 +10,6 @@ class App : Application() {
     override fun onCreate() {
         appComponent = DaggerMainComponent.builder().application(this).build()
         super.onCreate()
+        ANRWatchDog(3000).start()
     }
 }
