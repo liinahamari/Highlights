e916cbcf247a1f681aa65c84a72fafb2d52b5b25
liinahamari <l1maginaire00@gmail.com>
2024-01-12 19:47:17 +0400

    Make movie search request more strict to movie search

--- feature/suggestions/impl/src/main/java/dev/liinahamari/suggestions/impl/data/apis/SearchMovieApi.kt
+++ feature/suggestions/impl/src/main/java/dev/liinahamari/suggestions/impl/data/apis/SearchMovieApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface SearchMovieApi {
-    @GET("search/multi")
+    @GET("search/movie")
     fun search(
         @Query("query") query: String,
         @Query("page") page: Int = 1,
