67275085a6673a66b206f602476334c196902ede
liinahamari <l1maginaire00@gmail.com>
2024-03-16 00:29:54 +0400

    Remove unnecessary tests

--- feature/entity-list/impl/src/test/java/com/example/impl/ExampleUnitTest.kt
+++ feature/entity-list/impl/src/test/java/com/example/impl/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.impl
 
+import dev.liinahamari.impl.domain.RestoreDatabaseUseCaseImpl
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +13,7 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+        RestoreDatabaseUseCaseImpl(mock(), mock())
         assertEquals(4, 2 + 2)
     }
 }
